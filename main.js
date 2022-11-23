54c8c8f6a2c30e7e4a290b03ab6fbaff9f1bc9ae
votuanhuy2910 <votuanhuy2910@gmail.com>
2022-11-24 00:00:03 +0700

    update-click-bg

--- main.js
+++ main.js
@@ -56,23 +56,80 @@ window.onscroll = () => {
 let cursor1 = document.querySelector('.cursor-1');
 let cursor2 = document.querySelector('.cursor-2');
 
-window.onmousemove = (e) =>{
+window.onmousemove = (e) => {
     cursor1.style.top = e.pageY + 'px';
     cursor1.style.left = e.pageX + 'px';
     cursor2.style.top = e.pageY + 'px';
     cursor2.style.left = e.pageX + 'px';
 }
 
-document.querySelectorAll('a').forEach(links =>{
+document.querySelectorAll('a').forEach(links => {
 
-    links.onmouseenter = () =>{
+    links.onmouseenter = () => {
         cursor1.classList.add('active');
         cursor2.classList.add('active');
     }
 
-    links.onmouseleave = () =>{
+    links.onmouseleave = () => {
         cursor1.classList.remove('active');
         cursor2.classList.remove('active');
     }
 
 });
+
+let search = document.querySelectorAll('.fa-eye-slash');
+let description = document.querySelectorAll('.portfolio .box-container .box .description');
+
+search[0].onclick = () => {
+    search[0].classList.add('fa-eye');
+    description[0].classList.add('active');
+}
+
+search[1].onclick = () => {
+    search[1].classList.add('fa-eye');
+    description[1].classList.add('active');
+}
+
+search[2].onclick = () => {
+    search[2].classList.add('fa-eye');
+    description[2].classList.add('active');
+}
+
+search[3].onclick = () => {
+    search[3].classList.add('fa-eye');
+    description[3].classList.add('active');
+}
+
+let inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+    parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
+
+let theme = document.querySelector('#theme-toggler');
+let toggle = document.querySelector('.theme');
+let bg = document.querySelector('.bg');
+
+theme.onclick = () => {
+    toggle.classList.toggle('active');
+    bg.classList.toggle('active');
+}
+
+let themeColor = document.querySelectorAll('.bg span');
+    themeColor.forEach(color => color.addEventListener('click', () =>{
+        let background = color.style.background;
+        document.querySelector('body').style.background = background;
+}));
